c89ed3e7032a8992987f81a696450840ebfe2569
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-06-12 23:50:08 +0300

    [1431] [Time Beats: 100.00%] [Memory Beats: 28.99%] - LeetPush

---
+++ 1431-Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result;
+        int max = 0;
+        for(int i = 0;i<candies.size();i++)
+        {
+            if (candies[i]>max)
+            {
+                max = candies[i];
+            }
+        }
+
+        for(int j = 0;j<candies.size();j++)
+        {
+            if (candies[j]+extraCandies>=max)
+                result.push_back(true);
+            else{
+                result.push_back(false);
+            }
+        }
+        return result;
+    }
+};
