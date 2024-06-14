e09cae0543331b6de87caca73446952fef610dde
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-06-14 09:54:16 +0300

    [1365] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> result;
+        for(int  i = 0;i<nums.size();i++)
+        {
+            int counter = 0;
+            for(int j = 0;j<nums.size();j++)
+            {
+                
+                if (nums[i]>nums[j])
+                {
+                    counter++;
+                }
+            }
+            result.push_back(counter);
+        }
+        return result;
+    }
+};
