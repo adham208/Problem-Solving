dd730ddb35693ffd97e263a3f661e71c2b54f15b
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-10-21 14:22:37 +0300

    [1470] [Time Beats: 76.40%] [Memory Beats: 31.62%] - LeetPush

--- 1470-Shuffle-the-Array.cpp
+++ 1470-Shuffle-the-Array.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> result;
+        int left = 0;
+        int right = n;
+        vector<int> res;
+        for(int i = 0;i<n;i++){
+            res.push_back(nums[left]);
+            res.push_back(nums[right]);
+            left++;
+            right++;
 
-        
-
-        for(int i = 0; i<n;++i)
-        {
-            int first = nums[i];
-            int second = nums[n+i];
-            result.push_back(first);
-            result.push_back(second);
-            
         }
-        return result;
+        return res;
     }
 };
