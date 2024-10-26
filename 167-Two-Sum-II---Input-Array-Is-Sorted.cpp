90d2865110a8d03189501a2e18088d1eec54a60b
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-10-26 15:13:04 +0300

    [167] [Time Beats: 100.00%] [Memory Beats: 15.77%] - LeetPush

---
+++ 167-Two-Sum-II---Input-Array-Is-Sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size()-1;
+        while(right>left)
+        {
+            if(numbers[right]+numbers[left]==target)
+            {
+                return{left+1,right+1};
+            }
+            else if(numbers[right]+numbers[left]>target)
+            {
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return{};
+    }
+};
