ab5cc5b56734a138594d49dbfb59d0a1c96820e4
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-11-02 18:08:32 +0200

    [20] [Time Beats: 100.00%] [Memory Beats: 11.08%] - LeetPush

--- 20-Valid-Parentheses.cpp
+++ 20-Valid-Parentheses.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map <char,char>map = {
+        unordered_map<char,char> map = {
             {')','('},
             {']','['},
-            {'}','{'},
+            {'}','{'}
         };
         stack<char> st;
-        
-        for(int i = 0;i<s.size();i++){
-            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+
+        for(int i = 0;i<s.size();i++)
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+            {
                 st.push(s[i]);
             }
-            else{
-                if(map.count(s[i]))
+            else if(map.count(s[i]))
+            {
+                if(st.empty()||st.top()!=map[s[i]])
                 {
-                    if(st.empty()||st.top()!=map[s[i]])
-                    {
-                        return false;
-                    }
-                    st.pop();
+                    return false;
                 }
+                st.pop();
             }
+            
         }
         return st.empty();
-        
     }
 };
