71c286b6325ce8103daa8c2418a8057dc22f54b2
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-11-06 23:11:02 +0200

    [704] [Time Beats: 100.00%] [Memory Beats: 23.10%] - LeetPush

--- 704-Binary-Search.cpp
+++ 704-Binary-Search.cpp
@@ -4,15 +4,16 @@ public:
         int low = 0;
         int high = nums.size()-1;
 
-        while(low<=high){
-            int mid = low+(high-low)/2;
-
-            if(nums[mid]>target){
-                high = mid -1;
-            }
-            else if(nums[mid]==target)
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)
             {
                 return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                high = mid-1;
             }
             else{
                 low = mid+1;
