395b5421a12b766fd8edbbad4c32821a8dd5141d
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2024-12-03 23:46:47 +0200

    [1] [Time Beats: 78.60%] [Memory Beats: 13.08%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //number 1 + number 2 = target
         unordered_map<int,int> map;
 
         for(int i = 0;i<nums.size();i++)
         {
-            int ans = target - nums[i];
-            if(map.count(ans)){
-                return {map[ans],i};
+            int complement = target - nums[i];
+            if(map.find(complement)!= map.end())
+            {
+                return {map[complement],i};
             }
             else{
                 map.insert({nums[i],i});
             }
-            
         }
-        return{};
+        return {};
     }
 };
