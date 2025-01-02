0bc862c8900e784e496f73b2b2d6fa06c313cc38
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2025-01-02 09:25:25 +0200

    [121] [Time Beats: 100.00%] [Memory Beats: 17.88%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.cpp
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int left = 0;
-        int right = 0;
-        int maxP = 0;
+        int right = 1;
+        int maxProfit = 0;
 
         while(right<prices.size())
         {
-            maxP = max(maxP,prices[right]-prices[left]);
-
-            if(prices[right]<prices[left]){
+            maxProfit = max(maxProfit,prices[right]-prices[left]);
+            if(prices[right]<prices[left])
+            {
                 left = right;
             }
             right++;
         }
-        return maxP;
+        return maxProfit;
     }
 };
