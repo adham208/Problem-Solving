3cbcc32d2471748f3aa5277492822417944ee075
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2025-01-28 00:11:41 +0200

    [141] [Time Beats: 97.44%] [Memory Beats: 54.38%] - LeetPush

--- 141-Linked-List-Cycle.cpp
+++ 141-Linked-List-Cycle.cpp
@@ -9,17 +9,21 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* fast = head;
+        
         ListNode* slow = head;
-        while(fast &&fast->next)
+        ListNode* fast = head;
+
+        while(fast && fast->next)
         {
             fast = fast->next->next;
             slow = slow->next;
-            if(fast == slow)
+
+            if(slow == fast)
             {
                 return true;
             }
         }
+
         return false;
     }
 };
