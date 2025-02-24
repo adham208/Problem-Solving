50dbab8b4f498d1c70c4a5249ea93616151126a9
Adham Mahmoud <adhammahmoud2008@hotmail.com>
2025-02-25 00:24:53 +0200

    [49] [Time Beats: 74.13%] [Memory Beats: 56.62%] - LeetPush

--- 49-Group-Anagrams.cpp
+++ 49-Group-Anagrams.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> map;
-        for(auto i : strs)
+        for(int i = 0;i<strs.size();i++)
         {
-            string s = i;
-            sort(s.begin(), s.end());
-            map[s].push_back(i);
+            string st = strs[i];
+            sort(st.begin(),st.end());
+            map[st].push_back(strs[i]);
         }
 
         vector<vector<string>> res;
-
-        for(auto i : map)
+        for(auto i: map)
         {
             res.push_back(i.second);
         }
-
         return res;
-        
+
     }
 };
